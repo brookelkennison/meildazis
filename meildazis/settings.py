77ec717390d757f76a70ebe67467232ed02f5d23
Brooke Kennison <brookelkennison@gmail.com>
2019-10-06 18:13:48 -0500

    removed DIRS

--- meildazis/settings.py
+++ meildazis/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'meildazis.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'meildazis/templates')],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
